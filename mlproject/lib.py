746b3af4a1c545271469854860cc0ceb6fe5f22c
Ghislain Vigoureux <ghislain.vigoureux@gmail.com>
2021-03-15 14:10:49 +0100

    Changed script

--- mlproject/lib.py
+++ mlproject/lib.py
@@ -4,4 +4,4 @@ def hello_world():
     return "Hello world from mlproject"
 
 def try_me():
-    return "Error, your PC is infected by a virus, erasing in progress"
+    return "Hello from the other siiiiiiiiiide"
